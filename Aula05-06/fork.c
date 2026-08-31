0b996cc6d5e073df2260c70d0f7b5060d52a6424
Laurima104 <gamer11009h@gmail.com>
2026-08-31 11:24:48 -0300

    feat: implementado as funções do tema B da atividade referente a aula 05-06

--- Aula05-06/fork.c
+++ Aula05-06/fork.c
@@ -3,22 +3,46 @@
 #include <sys/types.h>
 #include <sys/wait.h> 
 
+//fazendo ativ B
 int main(void){
     for (int i = 0;  i < 3; i++){
         pid_t pid = fork();
+        int n = 20;
 
         if (pid < 0){
             perror("Ocorreu um erro");
             return 1;
         } else if(pid == 0) {
-            printf("Filho %d, PID=%d\n", i, pid);
+            switch (i)
+            {
+            case 0:
+                int sum=1;
+                for(int ip = 0; ip <= n; ip++){
+                    sum += n;
+                }
+                printf("Filho %d, PID=%d, Soma:%d\n", i, getpid(), sum);
+                break;
+            case 1:
+                int par=0;
+                for (int ip = 0; ip <= n; ip++){
+                    if(ip%2==0){
+                        par++;
+                    }
+                }
+                printf("Filho %d, PID=%d, Numero de Pares:%d\n", i, getpid(), par);
+                break;
+            case 2:
+                int pot = n*n;
+                printf("Filho %d, PID=%d, Potencia:%d\n", i, getpid(), pot);
+                break;
+            }
             return 0;
         }
     }
     for (int i = 0; i < 3; i++){
         wait(NULL);
     }
-    printf("Pai: todos Terminaram");
+    printf("Pai: todos Terminaram\n");
     return 0;
     
 }
